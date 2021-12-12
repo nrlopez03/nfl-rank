5bea89511e7b55ddf9fec6ce257f9fe4e0e4e8d8
Nicholas <nicholaslopez@college.harvard.edu>
2021-12-12 16:36:12 -0500

    customize

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
 def homepage():
-    return render_template("index.html", title="HOME PAGE")
+    return render_template("index.html", title="NFL Rank")
 
 if __name__ == "__main__":
     app.run(debug=True)
