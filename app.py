ed17ba4201852a3ee881ef0b7717c0b557152e3d
Nicholas <nicholaslopez@college.harvard.edu>
2021-12-12 15:41:45 -0500

    cleaned

--- app.py
+++ app.py
@@ -9,13 +9,5 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 def homepage():
     return render_template("index.html", title="HOME PAGE")
 
-@app.route("/docs")
-def docs():
-    return render_template("page.html", title="docs page")
-
-@app.route("/about")
-def about():
-    return render_template("page.html", title="about page")
-
 if __name__ == "__main__":
     app.run(debug=True)
